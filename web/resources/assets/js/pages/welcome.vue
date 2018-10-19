c6abbb1ec0a28c3ef6054913d4e67342b17c4f07
andreydiveev <flat-sea@ya.ru>
2018-10-19 23:15:40 +0200

    Update welcome.vue

--- web/resources/assets/js/pages/welcome.vue
+++ web/resources/assets/js/pages/welcome.vue
@@ -1,21 +1,5 @@
 <template>
   <div>
-    <div class="top-right links">
-      <template v-if="authenticated">
-        <router-link :to="{ name: 'home' }">
-          {{ $t('home') }}
-        </router-link>
-      </template>
-      <template v-else>
-        <router-link :to="{ name: 'login' }">
-          {{ $t('login') }}
-        </router-link>
-        <router-link :to="{ name: 'register' }">
-          {{ $t('register') }}
-        </router-link>
-      </template>
-    </div>
-
     <div class="text-center">
       <div class="title mb-4">
         {{ title }}
